66e9f303e398be390f069323d7d7d1fd58132710
Mozadded Ul Islam Noor <pmozadded@gmail.com>
2025-07-12 20:37:01 +0600

    backend resolved

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,10 @@
+// next.config.js
+import next from 'next';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone', // or 'export' for static sites
+  // Other config here
+};
 
 export default nextConfig;
